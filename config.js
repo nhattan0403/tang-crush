cbddacdc03d6e03288e4677eb651890391e8c4f8
nhattan0403 <85331886+nhattan0403@users.noreply.github.com>
2021-06-04 12:54:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const CONFIG = {
     introTitle: 'Thảo My ơi!',
     introDesc: `My xinh đẹp
     My dễ thương
-    My đáng yêu,
+    My đáng yêu`,
     btnIntro: 'Bấm vô nè',
     title: 'Anh yêu em 😙',
     desc: 'Anh nhớ em, yêu anh khônggg ',
     btnYes: 'Dạ yêuuuu <33',
     btnNo: 'Đéo yêu :3',
-    question:' Yêu anh honggg <3',
+    question:'Yêu anh honggg <3',
     btnReply: 'Gửi cho Tân đẹp trai',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
